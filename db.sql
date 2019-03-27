0889d7aa47429e2e4ad89ea52dc6bc191266ed64
Aisalkyn Dzhenalieva <aidzhenalieva@gmail.com>
2019-03-28 02:07:03 +0600

    db.sql database commands created and added

---
+++ db.sql
@@ -0,0 +1,47 @@
+CREATE SCHEMA `lab` DEFAULT CHARACTER SET utf8;
+
+USE `lab`;
+
+CREATE TABLE `categories` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(255) NOT NULL,
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`));
+
+  CREATE TABLE `places` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `place` VARCHAR(255) NOT NULL,
+    `description` TEXT NULL,
+    PRIMARY KEY (`id`));
+
+CREATE TABLE `items` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category_id` INT NOT NULL,
+    `place_id` INT NOT NULL,
+  `item` VARCHAR(255) NOT NULL,
+  `description` TEXT NULL,
+  `image`  VARCHAR(255) NULL,
+  `date`  DATE NULL,
+  PRIMARY KEY (`id`),
+  INDEX `category_id_idx` (`category_id` ASC),
+  INDEX `place_id_idx` (`place_id` ASC),
+  CONSTRAINT `category_id`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `categories` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `place_id`
+    FOREIGN KEY (`place_id`)
+    REFERENCES `places` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+INSERT INTO `categories` (`category`, `description`)
+VALUES ('furniture',  'just furniture'), ('computer equipment', 'computer details'), ('household appliances', 'appliances');
+
+INSERT INTO `places` (`place`, `description`)
+VALUES ('classroom 102', 'some description'),('computer class', 'some description'), ('teachers room', 'some description');
+
+INSERT INTO `items` (`id`, `item`, `date`, `description`, `category_id`, `place_id`)
+VALUES (1, 'laptop', '2019-03-27', 'a personal laptop', 3, 3), (2, 'table', '2019-03-27', 'round table', 1, 2), (3, 'chair', '2019-03-27', 'wooden chair', 1, 1);
